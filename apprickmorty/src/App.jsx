15ff703eb6b2495970aa913c862a51a0b2eae34d
santi19999 <santiherrera1999@gmail.com>
2022-01-22 23:19:25 -0300

    Agregue local Storage

--- apprickmorty/src/App.jsx
+++ apprickmorty/src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Formulario from './components/Formulario';
 import MostrarDatos from './components/MostrarDatos';
 
 const App = () => {
 	const [nombrePersonaje, setNombrePersonaje] = useState('');
+	useEffect(() => {
+		if (localStorage.getItem('nombreApi')) {
+			setNombrePersonaje(JSON.parse(localStorage.getItem('nombreApi')));
+		}
+	}, []);
+	useEffect(() => {
+		localStorage.setItem('nombreApi', JSON.stringify(nombrePersonaje));
+	}, [nombrePersonaje]);
 	return (
 		<div className="my-4 text-center">
 			<h1>App Rick and Morty</h1>
